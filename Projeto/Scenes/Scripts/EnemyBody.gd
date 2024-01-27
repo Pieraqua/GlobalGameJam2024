56a86b9beadd9126f88fced303fac7040bd2c73d
pieraqua <pauloffxii@hotmail.com>
2024-01-27 08:15:52 -0300

    Movimento do personagem, movimento dos inimigos, colisões

--- Projeto/Scenes/Scripts/EnemyBody.gd
+++ Projeto/Scenes/Scripts/EnemyBody.gd
@@ -1,21 +1,17 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+
+const SPEED = 100.0
 
 @onready var Globals = get_node("/root/MainScene")
+@onready var player = Globals.get("player")
 
-func _ready():
-	Globals.set("player", self)
 
 func _physics_process(_delta):
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction = global_position.direction_to(player.global_position)
 	if direction:
 		velocity = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
