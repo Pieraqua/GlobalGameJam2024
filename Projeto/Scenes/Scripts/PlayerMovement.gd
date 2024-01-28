c83ab4e5259214297cbbd8d8cb9118b6273841ad
Zeni <gustavo.zeni@pier.digital>
2024-01-28 04:04:57 -0300

    create sword w animation

--- Projeto/Scenes/Scripts/PlayerMovement.gd
+++ Projeto/Scenes/Scripts/PlayerMovement.gd
@@ -8,6 +8,8 @@ var damage_sources : Array[int] = []
 
 @onready var Globals = get_node("/root/MainScene")
 @onready var UI_HP = get_node("/root/MainScene/UI/Container_pontos/HPBar")
+@onready var sword = get_node("/root/MainScene/Mapa1/Player/CharacterBody2D/Espada")
+@onready var sword_animation_player = sword.get_node("SwordAnimationPlayer")
 
 func _ready():
 	Globals.set("player", self)
@@ -15,12 +17,22 @@ func _ready():
 	UI_HP.value = hitpoints
 
 func _process(delta):
+	var mouse_direction: Vector2 = (get_global_mouse_position() - global_position).normalized()
+	
 	if !damage_sources.is_empty():
 		hitpoints -= damage_sources.max()*delta
 		UI_HP.value = hitpoints
 		if hitpoints < 0:
 			print("Fim de jogo!")
 			get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+	
+	sword.rotation = mouse_direction.angle()
+	if sword.scale.y == 1 and mouse_direction.x < 0:
+		sword.scale.y = -1
+	elif sword.scale.y == -1 and mouse_direction.x >= 0:
+		sword.scale.y = 1
+	if Input.is_action_just_pressed("ui_attack") and not sword_animation_player.is_playing():
+		sword_animation_player.play("attack")
 
 func _physics_process(_delta):
 
