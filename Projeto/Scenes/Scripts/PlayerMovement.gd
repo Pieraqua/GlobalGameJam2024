e162cbbde7f00f7489a0cac3a52b05e8320bac81
Pieraqua <pieraquara@gmail.com>
2024-01-28 11:08:00 -0300

    prioridade animação

--- Projeto/Scenes/Scripts/PlayerMovement.gd
+++ Projeto/Scenes/Scripts/PlayerMovement.gd
@@ -31,15 +31,14 @@ func _physics_process(_delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if direction:
 		velocity = direction * SPEED
-		if direction.y > 0:
-			_animated_sprite.play("run_forward")
-		elif direction.y < 0:
-			_animated_sprite.play("run_back")
-		elif direction.x > 0:
+		if direction.x > 0:
 			_animated_sprite.play("run_right")
 		elif direction.x < 0:
 			_animated_sprite.play("run_left")
-			
+		elif direction.y > 0:
+			_animated_sprite.play("run_forward")
+		elif direction.y < 0:
+			_animated_sprite.play("run_back")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
