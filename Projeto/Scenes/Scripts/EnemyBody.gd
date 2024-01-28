b2c2e3d5375c58a911b1c2e771bac1cb17281ab6
Pieraqua <pieraquara@gmail.com>
2024-01-28 11:52:31 -0300

    Tilemaps para o mapa

--- Projeto/Scenes/Scripts/EnemyBody.gd
+++ Projeto/Scenes/Scripts/EnemyBody.gd
@@ -5,14 +5,31 @@ const ATK = 5
 const HP_MAX = 10
 
 var hitpoints = HP_MAX
+var dead = false
 
 @onready var Globals = get_node("/root/MainScene")
 @onready var player = Globals.get("player")
 
+# death animation frames and fps
+const N_FRAMES = 17.0
+const FPS = 10.0
+
+func _ready():
+	$AnimatedSprite2D.play("walk")
+	$DeathTimer.timeout.connect(really_die)
+	
+
 func die():
+	remove_child($CollisionShape2D)
+	$AnimatedSprite2D.play("defeat")
+	$DeathTimer.start(N_FRAMES/FPS)
+	dead = true
+	
+	
+func really_die():
 	var parent = get_parent()
 	get_parent().get_parent().remove_child(parent)
-	pass
+	
 	
 func hit(damage):
 	if(hitpoints <= damage):
@@ -24,7 +41,11 @@ func _physics_process(_delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = global_position.direction_to(player.global_position)
-	if direction:
+	if direction and !dead:
 		velocity = direction * SPEED
-
+		if direction.x > 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+			
 	move_and_slide()
